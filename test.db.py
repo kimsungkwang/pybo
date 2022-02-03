fc32718bd8542e329079e312811d9c0e5f2a4270
kimsungkwang <skk405@naver.com>
2022-02-04 01:13:49 +0900

    파이보 웹사이트

---
+++ test.db.py
@@ -0,0 +1,9 @@
+print("hello")
+
+from pybo.models import Question
+from django.utils import timezone
+
+for i in range(300):
+    q = Question(subject=f"테스트 데이터: {i:03d}",
+        content='내용무', create_data = timezone.now())
+    q.save()
